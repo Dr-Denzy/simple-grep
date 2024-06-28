d54c9a62286428c063783ebb193ad84f068856fe
Dennis Akpenyi <dennisakpenyi@gmail.com>
2024-06-28 10:39:42 +0200

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,57 @@
-use clap::{arg, Parser};
+use anyhow::{Context, Result};
+use clap::Parser;
+
 
 /// Search for a pattern in a file and display the lines that contain it
 #[derive(Parser)]
-struct Cli {
+pub struct Cli {
     /// the pattern to search for
-    query: String,
+    pub query: String,
     /// the path to the file to read
-    file_path: std::path::PathBuf,
+    pub file_path: std::path::PathBuf,
 }
 
+// #[derive(Debug)]
+// struct CustomError(String);
 
-fn main() {
-    // let query = std::env::args().nth(1).expect("no query or search pattern provided");
-    // let file_path = std::env::args().nth(2).expect("no file path provided");
-    //
-    // println!("pattern: {:?}, file_path: {:?}", query, file_path);
-    //
-    // let args = Cli {
-    //     query,
-    //     file_path: std::path::PathBuf::from(file_path),
-    // };
-
-    let args = Cli::parse();
-
-    println!("pattern: {:?}, file_path: {:?}", args.query, args.file_path);
-
-    let content = std::fs::read_to_string(args.file_path).expect("could not read file");
-
-
-    print!("Hits: \n");
+pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
     for line in content.lines() {
-        if line.contains(&args.query) {
-            println!("{line}");
+        if line.contains(pattern) {
+            // writeln!(writer, "{}", line).expect("TODO: panic message");
+            writeln!(writer, "{}", line).context("could not write to writer")?;
         }
     }
+    Ok(())
 }
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.file_path)
+        .with_context(|| format!("could not read file `{:?}`", args.file_path.display()))?;
+
+    print!("Hits: \n");
+
+    // for line in content.lines() {
+    //     if line.contains(&args.query) {
+    //         println!("{line}");
+    //     }
+    // }
+
+    find_matches(&content, &args.query, &mut std::io::stdout().lock())?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::find_matches;
+
+    #[test]
+    fn find_a_match() {
+        let mut result = Vec::new();
+        find_matches("It is a beautiful day!\nLet us rejoice!", "beautiful", &mut result).unwrap();
+        assert_eq!(result, b"It is a beautiful day!\n");
+    }
+}
+
